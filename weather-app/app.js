74919949e0bd2a03fc398d0b47fc41663fce70f7
Shivam Singla <singlashivam1000@gmail.com>
2026-01-03 12:05:03 +0530

    Getting data from http response -32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url ='https://api.weatherstack.com/current?access_key=09a3e384cd5aff612f6efd839382b7c3&query=New%20York'
+
+const url ='https://api.weatherstack.com/current?access_key=09a3e384cd5aff612f6efd839382b7c3&query=New%20York&units=f'
 
 request({ url: url , json: true} , (error , response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+    console.log(response.body.current.weather_descriptions[0] + " .Current Temperature in " + response.body.request.query + " is " + response.body.current.temperature + " and chances of getting rain is " + response.body.current.precip + "%")
 })
