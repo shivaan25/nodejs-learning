3e301d3b760f12571ffc302ec31e6c138ef4f5f8
Shivam Singla <singlashivam1000@gmail.com>
2026-01-04 22:28:00 +0530

    Creating routes for the app and passing html and json data  -44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,13 +3,19 @@ const express = require('express')
 const app = express()
 
 app.get('' ,(req,res) =>{
-    res.send('Hello Express!')
+    res.send('<h1>Weather</h1>')
 
 })
 
 
 app.get('/help' ,(req,res) => {
-    res.send('Help Page')
+    res.send([{
+        name:'Shivam',
+        age: 26
+    },{
+        name:'Parv',
+        age:21
+    }])
 })
 
 app.get('/about' ,(req,res) => {
