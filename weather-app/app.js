384b017cadf4f783b93fcdd770072648fb3c1e72
Shivam Singla <singlashivam1000@gmail.com>
2026-01-04 16:46:16 +0530

    Callback chaining -38

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,17 @@
 const geocode = require('../weather-app/utils/geocode')
 const weatherData = require('../weather-app/utils/weatherData')
 
-geocode('Panipat Haryana' ,(error , data) => {
-        console.log('Error' ,error)
-        console.log('Data' ,data)
+geocode('Delhi , India' ,(error , data) => {
+       if(error){
+        return console.log(error)
+       }
+       console.log('Location : ' + data.location)
+
+ weatherData(data.latitude ,data.longitude ,(error,forecastData) =>{
+    if(error){
+        console.log('Error',error)
+    }
+   
+    console.log(forecastData)
 })
-weatherData('Panipat Haryana' ,(error,data) =>{
-    console.log('Error',error)
-    console.log(data)
 })
--- weather-app/utils/weatherData.js
+++ weather-app/utils/weatherData.js
@@ -1,13 +1,13 @@
 const request = require('request')
 const chalk = require('chalk')
 
-const weatherData = (address ,callback) =>{
+const weatherData = (latitude,longitude,callback) =>{
     const weather_url ='https://api.weatherstack.com/current'
     request({
     url: weather_url,
     qs:{
         access_key:'09a3e384cd5aff612f6efd839382b7c3',
-        query:encodeURIComponent(address),
+        query:`${latitude},${longitude}`,
         units:''
     },
     json:true 
@@ -20,8 +20,8 @@ const weatherData = (address ,callback) =>{
         }else{
             callback(undefined,
                 response.body.current.weather_descriptions[0] +
-             " .Current Temperature in " + response.body.request.query + 
-             " is " + response.body.current.temperature + " and chances of getting rain is "
+             " .Current Temperature is "
+              + response.body.current.temperature + " and chances of getting rain is "
              + response.body.current.precip + "%"
             )
         }
