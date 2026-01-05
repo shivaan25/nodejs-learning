c55adf7e8282ec8f6b4873f13f342f122615eb55
Shivam Singla <singlashivam1000@gmail.com>
2026-01-05 14:59:13 +0530

    adding dynamic pages in the express server usinf express.render() -47

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,9 +5,33 @@ const express = require('express')
 const publicDirectory = path.join(__dirname,'../public')
 
 const app = express()
+app.set('view engine' , 'hbs')
 
 app.use(express.static(publicDirectory))
 
+
+app.get('' ,(req,res) =>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Shivam Singla'
+    })
+})
+
+
+app.get('/about',(req,res) =>{
+    res.render('about',{
+       title:'About',
+       name:'Ronnie Sleeping' 
+    })
+})
+
+app.get('/help',(req,res) =>{
+    res.render('help',{
+        title:'Help Page',
+        contact:'Shivam Singla',
+        address:'House no:242,NHBC,Panipat,Haryana'
+    })
+})
 
 
 app.get('/weather' ,(req,res) => {
