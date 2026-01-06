157d1c788243ac073d96b0b568b36aef9ba2fc99
Shivam Singla <singlashivam1000@gmail.com>
2026-01-06 15:44:20 +0530

    Weather App Completed and working , Section-8 completed  -59

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,20 +6,30 @@ console.log('Client Side javascript is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit' ,(e) =>{
     e.preventDefault()
 
     const location = search.value
 
+    messageOne.textContent='loading.....'
+    messageTwo.textContent= ''
     fetch('http://localhost:3000/weather?address='+ location)
 .then((res) =>{
     res.json()
     .then((data) =>{
         if(data.error){
-            console.log('Error:' + data.error)
+            
+            messageOne.textContent = data.error
+            
         }else{
-            console.log('Weather Forecate Data: ' + data.forecast)
+             messageOne.textContent=data.location
+                messageTwo.textContent= data.forecast
+            
         }
     })
 })
