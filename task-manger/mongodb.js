6692ff9267a1b5e7a0d75fe400d85fc231d2201d
Shivam Singla <singlashivam1000@gmail.com>
2026-01-09 22:25:38 +0530

    All About ObjectId in nosql database -77

--- task-manger/mongodb.js
+++ task-manger/mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient ,ObjectId} = require('mongodb')
 
 
 
@@ -6,6 +6,10 @@ const { MongoClient } = require('mongodb')
 
 const url = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task_manager'
+
+const id = new ObjectId()
+console.log(id)
+console.log(id.id.length)
 
 async function run() {
     console.log('Program Started!!')
@@ -21,22 +25,30 @@ async function run() {
         console.log('Connected to MongoDB')
 
         const database = client.db(databaseName)
-        const doc = [
-            {name:'Shivaan' ,age:26},
-            {name:'Rahul' ,age:17},
-            {name:'Rohit' ,age:20}
-        ]
+        // const doc = [
+        //     {name:'Shivaan' ,age:26},
+        //     {name:'Rahul' ,age:17},
+        //     {name:'Rohit' ,age:20}
+        // ]
+            //  await database.collection('users')
+            // .insertOne({
+            //     _id: id,
+            //     name:'Rohan',
+            //     age:30
+            // })
 
-        const insertManyEntries = await database.collection('users').insertMany(doc)
-        let ids = insertManyEntries.insertedIds
-        console.log(`${insertManyEntries.insertedCount} documents were inserted`)
-        for(let id of Object.values(ids)){
-            console.log('Inserted a Document with id: ' + id)
-        }
+        // const insertManyEntries = await database.collection('users').insertMany(doc)
+        // let ids = insertManyEntries.insertedIds
+        // console.log(`${insertManyEntries.insertedCount} documents were inserted`)
+        // for(let id of Object.values(ids)){
+        //     console.log('Inserted a Document with id: ' + id)
+        // }
 
 
-        const findEntries = await database.collection('users').find({}).toArray()
-        console.log(findEntries)
+        // const findEntries = await database.collection('users').find({
+        //     _id:id
+        // }).toArray()
+        // console.log(findEntries)
     } catch (e) {
         console.error(e.message)
         
