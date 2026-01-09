c0f45b958e795c46c68ad9ea897c089aa6c7b2b9
Shivam Singla <singlashivam1000@gmail.com>
2026-01-09 14:17:13 +0530

    Parctice Challenge to use inbuilt method in mongodb (insertMany,find,insertOne)

--- task-manger/mongodb.js
+++ task-manger/mongodb.js
@@ -1,5 +1,9 @@
 const { MongoClient } = require('mongodb')
 
+
+
+
+
 const url = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task_manager'
 
@@ -16,13 +20,23 @@ async function run() {
         await client.connect()
         console.log('Connected to MongoDB')
 
-        const db = client.db(databaseName)
+        const database = client.db(databaseName)
+        const doc = [
+            {name:'Shivaan' ,age:26},
+            {name:'Rahul' ,age:17},
+            {name:'Rohit' ,age:20}
+        ]
 
-        const result = await db.collection('users').insertOne({
-            name:'Parv',
-            age: 21
-        })
-        console.log('Insertion Completed!!')
+        const insertManyEntries = await database.collection('users').insertMany(doc)
+        let ids = insertManyEntries.insertedIds
+        console.log(`${insertManyEntries.insertedCount} documents were inserted`)
+        for(let id of Object.values(ids)){
+            console.log('Inserted a Document with id: ' + id)
+        }
+
+
+        const findEntries = await database.collection('users').find({}).toArray()
+        console.log(findEntries)
     } catch (e) {
         console.error(e.message)
         
