d0d623ab9f1d8c625ee83f753d01c4a594abb5c4
Shivam Singla <singlashivam1000@gmail.com>
2026-01-09 22:45:35 +0530

    using find and findMany to search the tasks that are not completed -78

--- task-manger/mongodb.js
+++ task-manger/mongodb.js
@@ -1,58 +1,32 @@
-const { MongoClient ,ObjectId} = require('mongodb')
+const { MongoClient, ObjectId } = require("mongodb");
 
+const url = "mongodb://127.0.0.1:27017";
+const databaseName = "task_manager";
 
-
-
-
-const url = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task_manager'
-
-const id = new ObjectId()
-console.log(id)
-console.log(id.id.length)
+const id = new ObjectId();
 
 async function run() {
-    console.log('Program Started!!')
+  console.log("Program Started!!");
 
+  try {
+    const client = new MongoClient(url, {
+      serverSelectionTimeoutMS: 3000,
+    });
 
-    try {
-        const client = new MongoClient(url,{
-            serverSelectionTimeoutMS: 3000,
-        })
+    await client.connect();
+    console.log("Connected to MongoDB");
 
+    const database = client.db(databaseName);
 
-        await client.connect()
-        console.log('Connected to MongoDB')
+    // const task = await database.collection('tasks').findOne({_id: new ObjectId("696131659db0ad3a70e04e2f")})
+    // console.log(task)
 
-        const database = client.db(databaseName)
-        // const doc = [
-        //     {name:'Shivaan' ,age:26},
-        //     {name:'Rahul' ,age:17},
-        //     {name:'Rohit' ,age:20}
-        // ]
-            //  await database.collection('users')
-            // .insertOne({
-            //     _id: id,
-            //     name:'Rohan',
-            //     age:30
-            // })
+    const tasksNotCompleted = await database.collection('tasks')
+    .find({ completed:false }).toArray()
 
-        // const insertManyEntries = await database.collection('users').insertMany(doc)
-        // let ids = insertManyEntries.insertedIds
-        // console.log(`${insertManyEntries.insertedCount} documents were inserted`)
-        // for(let id of Object.values(ids)){
-        //     console.log('Inserted a Document with id: ' + id)
-        // }
-
-
-        // const findEntries = await database.collection('users').find({
-        //     _id:id
-        // }).toArray()
-        // console.log(findEntries)
-    } catch (e) {
-        console.error(e.message)
-        
-    }
+    console.log(tasksNotCompleted)
+  } catch (e) {
+    console.error(e.message);
+  }
 }
-run()
-
+run();
