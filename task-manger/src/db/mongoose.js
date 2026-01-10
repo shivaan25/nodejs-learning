84e6d819e85bef022a0859093be42e5368acdcf8
Shivam Singla <singlashivam1000@gmail.com>
2026-01-10 23:37:15 +0530

    Completed the Challenge -84

--- task-manger/src/db/mongoose.js
+++ task-manger/src/db/mongoose.js
@@ -4,27 +4,51 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('Connection error:', err))
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true
-  },
-  title: String,
-  completed: {
-    type: Boolean,
-    default: false
-  }
+
+const Tasks = mongoose.model('Tasks' ,{
+    Body:{
+        type:String
+    },
+    completed:{
+        type:Boolean
+    }
+})
+console.log('Model Created!!!')
+
+
+const task = new Tasks({
+    Body:'I am compeleting 8 Videos today',
+    completed:true
 })
 
-const me = new User({
-  name: 'Parv',
-  title: 'I am a teacher'
-})
+ task.save().then((task) =>{
+    return console.log(task)
+ }).catch((error) =>{
+    console.log('Error ',error)
+ })
 
-me.save()
-  .then(user => {
-    console.log('Saved:', user)
-  })
-  .catch(error => {
-    console.log('ERROR:', error)
-  })
+
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   title: String,
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   }
+// })
+
+// const me = new User({
+//   name: 'Parv',
+//   title: 'I am a teacher'
+// })
+
+// me.save()
+//   .then(user => {
+//     console.log('Saved:', user)
+//   })
+//   .catch(error => {
+//     console.log('ERROR:', error)
+//   })
