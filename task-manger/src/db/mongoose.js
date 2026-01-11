6fd52afd6a82c53970eea9fb50f39a211b4653c7
Shivam Singla <singlashivam1000@gmail.com>
2026-01-11 15:03:46 +0530

    USINF MORE VALIDATION ON THE DATA -85

--- task-manger/src/db/mongoose.js
+++ task-manger/src/db/mongoose.js
@@ -1,54 +1,68 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const validator = require("validator");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/task-manager-api")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Connection error:", err));
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('Connection error:', err))
+// const Tasks = mongoose.model('Tasks' ,{
+//     Body:{
+//         type:String,
+//         required: true
+//     },
+//     completed:{
+//         type:Boolean
+//     }
+// })
+// console.log('Model Created!!!')
 
+// const task = new Tasks({
+//     Body:'I am compeleting 8 Videos today',
+//     completed:true
+// })
 
-const Tasks = mongoose.model('Tasks' ,{
-    Body:{
-        type:String
+//  task.save().then((task) =>{
+//     return console.log(task)
+//  }).catch((error) =>{
+//     console.log('Error ',error)
+//  })
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim:true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim:true,
+    lowercase:true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid");
+      }
     },
-    completed:{
-        type:Boolean
-    }
-})
-console.log('Model Created!!!')
+  },
+  title: {
+    type: String,
+    validate(value) {
+      if (value.length < 5) {
+        throw new Error("Title Should be less then 5 characters");
+      }
+    },
+  },
+});
 
+const me = new User({
+  name: "Shivaan 2     ",
+  email: "SHIVAMSINGLA222@GMAIL.COM",
+});
 
-const task = new Tasks({
-    Body:'I am compeleting 8 Videos today',
-    completed:true
-})
-
- task.save().then((task) =>{
-    return console.log(task)
- }).catch((error) =>{
-    console.log('Error ',error)
- })
-
-
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String,
-//     required: true
-//   },
-//   title: String,
-//   completed: {
-//     type: Boolean,
-//     default: false
-//   }
-// })
-
-// const me = new User({
-//   name: 'Parv',
-//   title: 'I am a teacher'
-// })
-
-// me.save()
-//   .then(user => {
-//     console.log('Saved:', user)
-//   })
-//   .catch(error => {
-//     console.log('ERROR:', error)
-//   })
+me.save()
+  .then((user) => {
+    console.log("Saved:", user);
+  })
+  .catch((error) => {
+    console.log("ERROR:", error);
+  });
