85413b9a87cd86c901cc7f7f2cb8ed5ca12fef18
Shivam Singla <singlashivam1000@gmail.com>
2026-01-12 14:07:36 +0530

    Adding Tasks Models and testing end points-2 -90

--- task-manger/src/db/mongoose.js
+++ task-manger/src/db/mongoose.js
@@ -4,20 +4,3 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/task-manager-api")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Connection error:", err));
-
-const Tasks = mongoose.model("Tasks", {
-  Body: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-console.log("Model Created!!!");
-
-const task = new Tasks({
-  Body: "I am compeleting 8 Videos today",
-});
--- task-manger/src/index.js
+++ task-manger/src/index.js
@@ -1,15 +1,28 @@
 const express = require('express')
-require('./db/mongoose')
+ require('./db/mongoose')
+
+
+
+const Tasks = require('./db/models/task')
 
 
 const User = require('./db/models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
-app.use(express.json())
 
 
+
 app.use(express.json())
+app.post('/task',(req,res) =>{
+    const task = new Tasks(req.body)
+
+    task.save().then(() =>{
+        res.status(201).send(task)
+    }).catch((e)=>{
+        res.status(400).send(task)
+    })
+})
 
 app.post('/users', (req, res) => {
     const user = new User(req.body)
