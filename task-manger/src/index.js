80ab0e8e00d701c7c4a5b9616ca00604c2413257
Shivam Singla <singlashivam1000@gmail.com>
2026-01-13 15:28:41 +0530

    Reading Api for task managment -91

--- task-manger/src/index.js
+++ task-manger/src/index.js
@@ -1,42 +1,65 @@
-const express = require('express')
- require('./db/mongoose')
+const express = require("express");
+require("./db/mongoose");
 
+const Tasks = require("./db/models/task");
 
+const User = require("./db/models/user");
 
-const Tasks = require('./db/models/task')
+const app = express();
+const port = process.env.PORT || 3000;
 
-
-const User = require('./db/models/user')
-
-const app = express()
-const port = process.env.PORT || 3000
-
-
-
-app.use(express.json())
-app.post('/task',(req,res) =>{
-    const task = new Tasks(req.body)
-
-    task.save().then(() =>{
-        res.status(201).send(task)
-    }).catch((e)=>{
-        res.status(400).send(task)
+app.use(express.json());
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
     })
-})
+    .catch((e) => {
+        res.status(500).send()
+    });
+});
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
 
-    user.save().then(() =>{
-       
+app.get('/users/:id',(req,res) => {
+    const _id = req.params.id
+
+
+    User.findById(_id).then((user) =>{
+        if(!user){
+            return res.status(404).send()
+        }
+
         res.send(user)
-    }).catch((e) =>{
-         res.status(400).send(e.message)
-        
+    }).catch((e)=>{
+        res.status(500).send()
     })
- 
 })
+app.post("/task", (req, res) => {
+  const task = new Tasks(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(task);
+    });
+});
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e.message);
+    });
+});
 
 app.listen(port, () => {
-  console.log('Server is running on : ', port)
-})
+  console.log("Server is running on : ", port);
+});
